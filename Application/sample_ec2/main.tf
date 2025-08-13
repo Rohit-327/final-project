c0fc72f55a125babaf2f1a8049f6fccf6fbc046b
ROHIT KUMBHAR <rohitk25032002>
2025-08-13 12:54:14 +0530

    update file

--- Application/sample_ec2/main.tf
+++ Application/sample_ec2/main.tf
@@ -1,5 +1,5 @@
 module "ec2_module" {
-  source = "../../module/ec2/v0"  # Path to your module
+  source = "../module/ec2/v0"  # Path to your module
   ami_id                    = var.ami_id
   instance_type          = var.instance_type
   key_name               = var.key_name
